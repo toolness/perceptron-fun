412900ab6665d91bd6e434cc8ef836412fc40487
Atul Varma <varmaa@gmail.com>
2025-03-08 17:53:51 -0500

    Visualize.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use macroquad::prelude::*;
+
 use perceptron::{Datapoint, Perceptron};
 
-mod vec3;
 mod perceptron;
+mod vec3;
 
-fn main() {
+#[macroquad::main("Perceptron Fun")]
+async fn main() {
     let datapoints: Vec<Datapoint> = vec![
-        Datapoint((3, 1), 1),
+        Datapoint((3, 1), -1),
         Datapoint((2, -1), 1),
         Datapoint((-2, 1), 1),
         Datapoint((-1, -3), -1),
@@ -14,9 +17,18 @@ fn main() {
     perceptron.set_datapoints(datapoints);
 
     loop {
-        let incorrect = perceptron.update();
-        println!("Finished update with {incorrect} incorrect.");
-        if incorrect == 0 {
+        clear_background(BLACK);
+
+        if is_key_pressed(KeyCode::Tab) {
+            perceptron.update();
+        }
+
+        draw_text("Press tab to update perceptron.", 0.0, 30.0, 30.0, WHITE);
+
+        perceptron.draw();
+        next_frame().await;
+
+        if is_key_pressed(KeyCode::Escape) {
             break;
         }
     }
--- src/perceptron.rs
+++ src/perceptron.rs
@@ -1,11 +1,12 @@
 use super::vec3::Vec3;
+use macroquad::prelude::*;
 
 pub struct Datapoint(pub (i32, i32), pub i32);
 
 #[derive(Default)]
 pub struct Perceptron {
     datapoints: Vec<Datapoint>,
-    weights: Vec3
+    weights: Vec3,
 }
 
 impl Perceptron {
@@ -16,7 +17,7 @@ impl Perceptron {
     pub fn update(&mut self) -> i32 {
         let mut incorrect = 0;
         for point in &self.datapoints {
-            let x = Vec3(1.0, point.0.0 as f64, point.0.1 as f64);
+            let x = Vec3(1.0, point.0 .0 as f64, point.0 .1 as f64);
             let y = point.1 as f64;
             let w_dot_x = self.weights.dot(&x);
             if y * w_dot_x <= 0.0 {
@@ -25,5 +26,53 @@ impl Perceptron {
             }
         }
         incorrect
+    }
+
+    pub fn draw(&self) {
+        const SCALE: f32 = 8.0;
+        let center_x = screen_width() / 2.0;
+        let center_y = screen_height() / 2.0;
+
+        let screen_x = |x: f32| -> f32 { center_x + x * SCALE };
+        let screen_y = |y: f32| -> f32 { center_y + y * SCALE };
+
+        // Draw axes
+        draw_line(0.0, center_y, screen_width(), center_y, 1.0, DARKGRAY);
+        draw_line(center_x, 0.0, center_x, screen_height(), 1.0, DARKGRAY);
+
+        // Draw points
+        for point in &self.datapoints {
+            draw_circle(
+                screen_x(point.0 .0 as f32),
+                screen_y(point.0 .1 as f32),
+                SCALE / 2.0,
+                if point.1 <= 0 { RED } else { GREEN },
+            );
+        }
+
+        draw_text(
+            &format!("Weights: {:?}", self.weights),
+            0.0,
+            screen_height() - 30.0,
+            30.0,
+            DARKBLUE,
+        );
+
+        // Draw weights (as line)
+        if self.weights.2 != 0.0 {
+            let f = |x: f32| -> f32 {
+                ((-self.weights.0 - self.weights.1 * x as f64) / self.weights.2) as f32
+            };
+            let x1 = -1000.0;
+            let x2 = 1000.0;
+            draw_line(
+                screen_x(x1),
+                screen_y(f(x1)),
+                screen_x(x2),
+                screen_y(f(x2)),
+                1.0,
+                BLUE,
+            );
+        }
     }
 }
