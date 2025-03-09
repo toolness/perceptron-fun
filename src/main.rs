d4cf575d2e730487595fa42e0d6019e174281bc2
Atul Varma <varmaa@gmail.com>
2025-03-09 09:31:15 -0400

    Press 'A' to auto-update.

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,34 @@ mod vec3;
 async fn main() {
     let mut perceptron = Perceptron::new(
         vec![
-            Datapoint::new((3, 1), 1),
+            Datapoint::new((3, 1), -1),
             Datapoint::new((2, -1), 1),
             Datapoint::new((-2, 1), 1),
             Datapoint::new((-1, -3), -1),
         ]
     );
 
+    let mut auto_update = false;
+
     loop {
         clear_background(BLACK);
 
-        if is_key_pressed(KeyCode::Space) {
+        if is_key_pressed(KeyCode::A) {
+            auto_update = !auto_update;
+        }
+
+        if is_key_pressed(KeyCode::Space) || auto_update {
             perceptron.update();
         }
 
         let status = if perceptron.has_converged() {
             "Perceptron has converged."
         } else {
-            "Press space to update perceptron."
+            if auto_update {
+                "Auto-updating perceptron (press 'A' to stop)."
+            } else {
+                "Press space to update perceptron (press 'A' to auto-update)."
+            }
         };
         draw_text(status, 0.0, 30.0, 30.0, WHITE);
 
